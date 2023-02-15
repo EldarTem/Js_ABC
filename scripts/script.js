260a8d99792a8ef5c1651e1618bef3845b523820
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-16 00:53:43 +0300

    Добавление элементов в объекты в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,5 @@
-let obj = {
-	'1a': 1,
-	'b2': 2,
-	'с-с': 3,
-	'd 4': 4,
-	'e5': 5
-};
-
-console.log(obj['1a']);
-console.log(obj.b2);
-console.log(obj['c-c']);
-console.log(obj['d 4']);
-console.log(obj.e5);
+let obj = {};
+obj.a = 1;
+obj.b = 2;
+obj.c = 3;
+console.log(obj);
