b2f1072265c9523eff83a6a344bdbea4803a600a
EldarTem <temirov.e.i.21@gmail.com>
2023-02-15 20:32:51 +0300

    Переменные в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,20 @@
-"use strict";
-alert('КИПУ');//какой-то комментарий
-/*
-еще один комментарий 
-*/
+let num = 123;
+alert(num);
+
+let a = 1;
+let b = 2;
+let c = 3;
+
+let x = 1,
+  y = 2,
+  z = 3;
+
+let d, e, f;
+d = 1;
+e = 2;
+f = 3;
+
+let n = 10;
+alert(n);
+n = 20;
+alert(n);
