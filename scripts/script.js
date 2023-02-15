c55c61bb4eb13daf9fcdeacc08a85ded963c0dd9
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-16 00:17:52 +0300

    Преобразование логического типа в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,7 @@
-let num1 = 11;
-let num2 = 22;
-
-alert(String(num1) + String(num2));
-
-let Num = 12345678910;
-alert(String(Num).length);
-
-
-let number = 1234567;
-let str2 = String(num);
-
-let number2 = 123;
-let str3 = String(num);
-alert(str2.length + str3.length); 
+alert(true + 3); 
+alert(true + true); 
+alert(true - true); 
+alert(true + false);
+alert('1' + true); 
+alert( String(true) + 1 ); 
+alert( String(true) + Number(true) ); 
