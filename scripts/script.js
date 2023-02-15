6fe85ab6e3a03afd00a2f111617f03d83f524618
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-15 23:36:06 +0300

    Математические операции с числами в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,2 @@
-let num = 123;
-alert(num);
-
-let a = 1;
-let b = 2;
-let c = 3;
-
-let x = 1,
-  y = 2,
-  z = 3;
-
-let d, e, f;
-d = 1;
-e = 2;
-f = 3;
-
-let n = 10;
-alert(n);
-n = 20;
-alert(n);
+let a = 5 + 8 + 16;
+alert(a);
