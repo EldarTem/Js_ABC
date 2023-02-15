079bbeaf0dddace21e9532609235989ae59489dd
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-16 00:22:11 +0300

    Получение символов строки на JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,62 +1,13 @@
-let test = Boolean(3);
-alert(test); 
+let str = 'abcde';
 
-let test1 = Boolean(0);
-alert(test1); 
+alert(str[0]); 
+alert(str[2]); 
+alert(str[4]); 
 
-let test2 = Boolean(-1);
-alert(test2); 
+let o = str[4]+ str[3]+ str[2]+ str[1]+ str[0];
+alert(o);
 
-let test3 = Boolean(-0);
-alert(test3); 
+let str = 'abcde';
+let num = 3; 
 
-let test4 = Boolean(+0);
-alert(test4);
-
-let test5 = Boolean('abc');
-alert(test5);
-
-let test6 = Boolean('');
-alert(test6); 
-
-let test7 = Boolean('0');
-alert(test7); 
-
-let test8 = Boolean(true);
-alert(test8); 
-
-let test9 = Boolean(false);
-alert(test9);
-
-let test10 = Boolean('true');
-alert(test10); 
-
-let test11 = Boolean('false');
-alert(test11); 
-
-let test12 = Boolean(null);
-alert(test12); 
-
-let test13 = Boolean('null');
-alert(test13); 
-
-let test14 = Boolean(undefined);
-alert(test14); 
-
-let test15 = Boolean('undefined');
-alert(test15); 
-
-let test16 = Boolean(NaN);
-alert(test16); 
-
-let test17 = Boolean('NaN');
-alert(test17); 
-
-let test18 = Boolean(3 * 'abc');
-alert(test18); 
-
-let test19 = Boolean(Infinity);
-alert(test19); 
-
-let test20 = Boolean(1 / 0);
-alert(test20); 
+alert(str[num]); 
