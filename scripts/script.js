9f264539e616a6101978b69290562bbc3c2f385c
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-16 00:42:10 +0300

    Изменение элементов массива в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,17 @@
-let arr = ['kipu', 'kfu', 'sevgu'];
+let arr = ['a', 'b', 'c'];
+arr[0] = '1';
+arr[1] = '2';
+arr[2] = '3';
+console.log(arr);
 
-console.log(arr.length) 
-console.log(arr[arr.length - 1])
+let arr1 = ['1', '2', '3'];
+arr1[0] += 3;
+arr1[1] += 3;
+arr1[2] += 3;
+console.log(arr1);
+
+let arr3 = [1, 2, 3];
+arr3[0]++;
+arr3[1]++;
+arr3[2]++;
+console.log(arr3);
