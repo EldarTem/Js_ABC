7348111d3824b9332942b1e38147d669717f488f
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-02-16 00:20:34 +0300

    Преобразование к логическому типу в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,62 @@
-alert(true + 3); 
-alert(true + true); 
-alert(true - true); 
-alert(true + false);
-alert('1' + true); 
-alert( String(true) + 1 ); 
-alert( String(true) + Number(true) ); 
+let test = Boolean(3);
+alert(test); 
+
+let test1 = Boolean(0);
+alert(test1); 
+
+let test2 = Boolean(-1);
+alert(test2); 
+
+let test3 = Boolean(-0);
+alert(test3); 
+
+let test4 = Boolean(+0);
+alert(test4);
+
+let test5 = Boolean('abc');
+alert(test5);
+
+let test6 = Boolean('');
+alert(test6); 
+
+let test7 = Boolean('0');
+alert(test7); 
+
+let test8 = Boolean(true);
+alert(test8); 
+
+let test9 = Boolean(false);
+alert(test9);
+
+let test10 = Boolean('true');
+alert(test10); 
+
+let test11 = Boolean('false');
+alert(test11); 
+
+let test12 = Boolean(null);
+alert(test12); 
+
+let test13 = Boolean('null');
+alert(test13); 
+
+let test14 = Boolean(undefined);
+alert(test14); 
+
+let test15 = Boolean('undefined');
+alert(test15); 
+
+let test16 = Boolean(NaN);
+alert(test16); 
+
+let test17 = Boolean('NaN');
+alert(test17); 
+
+let test18 = Boolean(3 * 'abc');
+alert(test18); 
+
+let test19 = Boolean(Infinity);
+alert(test19); 
+
+let test20 = Boolean(1 / 0);
+alert(test20); 
