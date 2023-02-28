462d5301bf871e4eef2bb577fa632b8d96225646
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-03-01 00:45:19 +0300

    Логические операции

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,14 @@
-let num = 1;
+// 1
+let a = 2 * (3 - 1);
+let b = 6 - 2;
+console.log(a == b);
 
-let res = (num >= 0) ? '1' : '2';
+// 2
+a = 5 * (7 - 4);
+b = 1 + 2 + 7;
+console.log(a > b);
 
-console.log(res);
+// 3
+a = 2 ** 4;
+b = 4 ** 2;
+console.log(a != b);
