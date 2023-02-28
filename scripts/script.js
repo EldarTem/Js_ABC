6d43c1bff6e18606a7ebb07db47e917ae4fe0b86
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-03-01 00:58:09 +0300

    Проверка частей часа через if-else

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,25 @@
-// 1
-let age = 17;
-let adult;
+let min = 10;
 
-if (age >= 18) {
-	adult = true;
-} else {
-	adult = false;
+if (min >= 0 && min <= 19) {
+  console.log("1 четверть");
 }
 
-console.log(adult);
-
-// 2
-let age2 = 17;
-let adult2;
-
-if (age2 >= 18) {
-	adult2 = true;
-} else {
-	adult2 = false;
+if (min >= 20 && min <= 39) {
+  console.log("2 четверть");
 }
 
-console.log(adult2);
+if (min >= 40 && min <= 59) {
+  console.log("3 четверть");
+  let min = 10;
 
-// 3
-let age3 = 17;
-let res;
-
-if (age3 >= 18) {
-	if (age3 <= 23) {
-		res = 'от 18 до 23';
-	} else {
-		res = 'больше 23';
-	}
-} else {
-	res = 'меньше 18';
-}
-
-console.log(res);
-
-// 4
-let age4 = 19;
-let res2;
-
-if (age4 >= 18) {
-	if (age4 <= 23) {
-		res2 = 'от 18 до 23';
-	} else {
-		res2 = 'больше 23';
-	}
-} else {
-	res2 = 'меньше 18';
-}
-
-console.log(res2);
+  if (min >= 0 && min <= 19) {
+	console.log("1 четверть");
+  }
+  
+  if (min >= 20 && min <= 39) {
+	console.log("2 четверть");
+  }
+  
+  if (min >= 40 && min <= 59) {
+	console.log("3 четверть");
+  }
