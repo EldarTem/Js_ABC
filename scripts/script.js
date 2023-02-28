dbc87a3f9fb7080566ab2d6ae961da2bb2fc4309
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-03-01 01:03:56 +0300

    Проверка символов строки

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
-let arr = [10, 23, 12];
-
-if (arr.length == 3) {
-  let sum = arr.map((i) => (x += i), (x = 0)).reverse()[0];
-  alert(sum);
+// TASK 1
+let str = "babvcadxa";
+if (str[0] == "a") {
+  console.log('Cтрока начинается на символ "a"');
 } else {
-  alert("В массиве не 3 значения");
+  console.log('Cтрока не начинается на символ "a"');
+}
+
+// TASK 2
+if (str[str.length - 1] == "x") {
+  console.log('Cтрока начинается на символ "x"');
+} else {
+  console.log('Cтрока не начинается на символ "x"');
+}
+
+// 3
+if (str[0] == "a" || str[0] == "b") {
+  console.log('Cтрока начинается на символ "a" или "b"');
+} else {
+  console.log('Cтрока не начинается на символ "a" или "b"');
 }
