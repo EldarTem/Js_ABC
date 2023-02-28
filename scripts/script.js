1da1e84fe58b95635e2f3c7f34c90ee41bbc0f4b
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-03-01 00:27:01 +0300

    Сокращенная форма проверки на ложь в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,19 @@
 let test = true;
+if (!test) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
 
-if (test) {
+let test1 = true;
+if (!test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let test2 = true;
+if (test2) {
 	console.log('+++');
 } else {
 	console.log('---');
