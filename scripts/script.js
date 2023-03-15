43e9eddf91bb9ff09643d7840b7a673dcb745deb
EldarTem <temirov.e.i.21@gmail.com>
2023-03-15 18:38:19 +0300

    Логические операторы без if в функциях JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,58 +1,33 @@
-let arr1 = [2, 4, 6];
+let x = 2, y = 2
 
-function func1(arr) {
-
-   for (let elem of arr) {
-      if (elem % 2 != 0) {
-         return false;
-      }
-   };
-
-   return true;
+function func(a, b) {
+   return a == b;
 };
 
-if (func1(arr1)) {
-   console.log('Все числа в массиве четные');
-} else {
-   console.log('В массиве есть нечетные числа')
+console.log(func(x, y));
+
+x = 3;
+y = 2;
+
+function func2(a, b) {
+   return a != b;
 }
 
-let num = 4663;
+console.log(func2(x, y));
 
-function func2(number) {
+x = 3;
+y = 3;
 
-   let arrNum = String(number).split('')
-
-   for (let elem of arrNum) {
-      if (elem % 2 != 0) {
-         return false;
-      }
-   };
-
-   return true;
-};
-
-if (func2(num)) {
-   console.log('Все цифры в числе четные');
-} else {
-   console.log('В числе есть нечетные цифры')
+function func3(a, b) {
+   return a + b >= 10;
 }
 
-let arr3 = [2, 3, 4, 4, 7];
+console.log(func3(x, y));
 
-function func3(arr) {
-   for (let i = 0; i <= arr.length - 1; i++) {
-      if (arr[i] == arr[i+1]) {
-         return true;
-      } else {
-         continue;
-      }
-   };
-   return false;
+let number = -2;
+
+function func4(num) {
+   return num >= 0
 }
 
-if (func3(arr3)) {
-   console.log('В этом масиве есть два одинаковых элемента подряд');
-} else {
-   console.log('В этом масиве нет двух одинаковых элемента подряд')
-}
+console.log(func4(number));
