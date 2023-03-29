e9591dc2bd2ea64cabb0e5cbe5492f1ebc22425a
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-03-29 10:13:47 +0300

    Советы по созданию функций в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,66 +1,58 @@
-let obj = {
-   key1: {
-      key1: 1,
-      key2: 2,
-      key3: 3,
-   },
-   key2: {
-      key1: 4,
-      key2: 5,
-      key3: 6,
-   },
-   key3: {
-      key1: 7,
-      key2: 8,
-      key3: 9,
-   },
- };
- 
- let sum1 = obj.key1.key1 + obj.key1.key2 + obj.key1.key3;
- let sum2 = obj.key2.key1 + obj.key2.key2 + obj.key2.key3;
- let sum3 = obj.key3.key1 + obj.key3.key2 + obj.key3.key3;
- 
- console.log(sum1 + sum2 + sum3)
- 
- let obj1 = {
-   1: {
-      1: 'a1',
-      2: 'a2',
-      3: 'a3',
-   },
-   2: {
-      1: 'b1',
-      2: 'b2',
-      3: 'b3',
-   },
-   3: {
-      1: 'c1',
-      2: 'c2',
-      3: 'c3',
-   },
- };
- 
- console.log(obj1[2][2]);
- console.log(obj1[3][1]);
- 
- let obj2 = {
-   key1: {
-      a: 1,
-      b: 2,
-      c: {
-         d: 3,
-         e: 4,
-      }, 
-      f: 5,
-   },
-   key2: {
-      g: 6,
-      h: 7,
-   },
- };
- 
- let sum11 = obj2.key1.a + obj2.key1.b + obj2.key1.c.d + obj2.key1.c.e + + obj2.key1.f;
- let sum22 = obj2.key2.g + obj2.key2.h;
- 
- 
- console.log(sum11 + sum22);
+let arr1 = [1, 2, 3, 4];
+
+      function calcArr(arr) {
+
+         let res = 0;
+
+         for (let elem of arr) {
+            res += elem;
+         };
+
+         res = res / arr.length
+
+         return res;
+      };
+
+      console.log(calcArr(arr1))
+
+   
+      
+
+
+
+      let arr2 = [1, 2, 3, 4];
+      let arr3 = [5, 6, 7, 8];
+
+      function sum(arr) {
+         let sum = 0;
+         for (let elem of arr) {
+            sum += elem
+         };
+         return sum;
+      };
+
+      function num1(arr1, arr2) {
+         let res1 = sum(arr1)
+         let res2 = sum(arr2)
+         return res1 / res2;
+      };
+
+      console.log(num1(arr2, arr3));
+
+
+      
+
+     
+      let arr6 = [1, 2, 3, 4, 5, 6]
+
+      function num2(arr) {
+         let res = 1;
+
+         for (let elem of arr) {
+            res *= elem;
+         }
+
+         return res;
+      };
+
+      console.log(num2(arr6));
