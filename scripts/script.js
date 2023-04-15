09959d752b83ac1ef41c9f1daec370de4a9d72af
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-04-15 19:17:07 +0300

    Исключения при работе с атрибутами в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
-let elem = document.querySelector("#elem");
+let div1 = document.querySelector("#elem1");
+let button1 = document.querySelector("#btn1");
 
-elem.addEventListener("focus", function () {
-   console.log(elem.value);
+button1.addEventListener("click", () => {
+   console.log(div1.className);
+});
+
+let div2 = document.querySelector("#elem2");
+let button2 = document.querySelector("#btn2");
+
+button2.addEventListener("click", () => {
+   div2.className = "main";
+});
+
+let div3 = document.querySelector("#elem3");
+let button3 = document.querySelector("#btn3");
+
+button3.addEventListener("click", () => {
+ let arr = [div3.classList[0], div3.classList[1], div3.classList[2]]
+ console.log(arr)
 });
