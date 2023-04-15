d70d66994bd4cb140c76195523fbe17531a07299
Eldar Temirov <temirov.e.i.21@gmail.com>
2023-04-15 19:41:05 +0300

    Объект this в JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,22 @@
-document.querySelector('#image').src = 'favicon.ico';
+let input = document.querySelector('#input');
 
- /*
- console.log(document.querySelector('#image2').src);
- console.log(document.querySelector('#image2').width);
- console.log(document.querySelector('#image2').height);
- Ошибка состоит в том, что из-за поиска 
- айди, браузер несколько раз выполняет одно и тоже действие несколько раз
- */
+input.addEventListener('focus', funcFocus);
 
- let img2 = document.querySelector('#image2');
- img2.src = 'favicon.ico'
- img2.height = '100'
- img2.width = '200'
+function funcFocus() {
+ this.value = '1';
+};
+
+input.addEventListener('blur', funcBlur);
+
+function funcBlur() {
+ this.value = '2';
+};
+
+let button = document.querySelector("#btn");
+
+button.addEventListener("click", addOne);
+
+function addOne() {
+ let sum = (Number(this.textContent) + 1 )
+ this.textContent = sum
+}
